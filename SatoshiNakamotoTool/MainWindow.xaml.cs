144c4cb31c90a0c87830d278a6e95810449a88cc
tomas <tomas@DESKTOP-JDM5F6I>
2018-07-21 04:57:57 +0300

    Push

--- SatoshiNakamotoTool/MainWindow.xaml.cs
+++ SatoshiNakamotoTool/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace SatoshiNakamotoTool
         {
             InitializeComponent();
         }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
